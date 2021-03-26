44bab550292acd6259c4bf22efa7916bedf4896f
domihorinka <domihorinka@gmail.com>
2021-03-26 11:07:12 -0600

    finished the routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,19 +1,41 @@
-const router = require("express").Router()
-const Workout = require("../models/workout")
+const router = require("express").Router();
+const Workout = require("../models/workout");
 
 router.get("/api/workouts",(req, res) => {
     Workout.find({}).then(data => {
         res.json(data)
     })
 });
-router.put("/api/workouts/:id",(req, res) => {
 
+router.put("/api/workouts/:id",({ body, params}, res) => {
+    Workout.findByIdAndUpdate(
+        params.id,
+        { $push: { exercises: body } },
+        { new: true, runValidators: true }
+      )
+        .then(workOut => {
+          res.json(workOut);
+        })
+        .catch(err => {
+          res.json(err);
+});
 });
 router.post("/api/workouts",(req, res) => {
-
+    Workout.create(req.body).then((workOut => {
+        res.json(workOut);
+    })).catch(err =>{
+        res.json(err);
+    })
+   
 });
+
 router.get("/api/workouts/range",(req, res) => {
-
+    Workout.find({}).limit(7)
+    .then((workOut) => {
+        res.json(workOut)
+    }).catch(err => {
+        res.json(err)
+    })
 });
 
-module.exports=router;
+module.exports= router;
